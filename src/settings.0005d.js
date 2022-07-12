11e5467a1db7e0a578a9a0664a3c4e1c4b169260
sushantGitH <69434965+sushantGitH@users.noreply.github.com>
2022-07-12 14:56:58 +0530

    contact

--- src/settings.0005d.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/resources/scene/maainmenu.fire",orientation:"",jsList:[],bundleVers:{internal:"45e63",resources:"35222",main:"03551"}};
