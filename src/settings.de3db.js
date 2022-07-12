b7f0a2655f20d47ff91683fc3f3b53a38e6c8d07
sushantGitH <69434965+sushantGitH@users.noreply.github.com>
2022-07-12 12:36:40 +0530

    contact me updated

---
+++ src/settings.de3db.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/resources/scene/maainmenu.fire",orientation:"",jsList:[],bundleVers:{internal:"45e63",resources:"f9432",main:"03551"}};
