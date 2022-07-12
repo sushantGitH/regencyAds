bf969e4f8613f0a6eeec41a7e8c76c02526b72d3
sushantGitH <69434965+sushantGitH@users.noreply.github.com>
2022-07-13 01:41:48 +0530

    changes

--- src/settings.0b94c.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/resources/scene/homeScene.fire",orientation:"",jsList:[],bundleVers:{internal:"45e63",resources:"60168",main:"03551"}};
