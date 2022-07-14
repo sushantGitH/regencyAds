7cd8f17cae2fe3708495031fefa4a0c8f8cc8465
sushantGitH <69434965+sushantGitH@users.noreply.github.com>
2022-07-14 20:39:57 +0530

    zoom added

---
+++ src/settings.04fa6.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/resources/scene/homeScene.fire",orientation:"",jsList:[],bundleVers:{internal:"45e63",resources:"24c56",main:"03551"}};
